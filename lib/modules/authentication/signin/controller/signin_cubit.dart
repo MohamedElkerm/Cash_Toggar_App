2c2587e624c4fd33bcc9d32c32906c767cc3d34d
mohamed elkerm personal laptop <mohamedelkerm584@gmail.com>
2025-01-25 20:00:20 +0200

    cache the uid in signin screen

--- lib/modules/authentication/signin/controller/signin_cubit.dart
+++ lib/modules/authentication/signin/controller/signin_cubit.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:cash_toggar_app/helper/local/cache_helper.dart';
+import 'package:cash_toggar_app/helper/local/cache_helper_keys.dart';
 import 'package:cash_toggar_app/helper/routing/app_routes.dart';
 import 'package:cash_toggar_app/helper/routing/router.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -21,45 +23,47 @@ class SigninCubit extends Cubit<SigninState> {
   TextEditingController passwordController = TextEditingController();
 
   bool isVisibile = false;
+
   void changeVisibility() {
     isVisibile = !isVisibile;
     emit(SigninVisibilityChangeState());
   }
 
-
   bool signinLoading = false;
-  Future<void> signInUsingFirebase({required context , required isArabic}) async {
+
+  Future<void> signInUsingFirebase(
+      {required context, required isArabic}) async {
     if (formKeyForSigninScreen.currentState!.validate()) {
       emit(SigninLoadingState());
       signinLoading = true;
 
-
       try {
         // Sign in with email and password
-        UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        UserCredential userCredential = await FirebaseAuth.instance
+            .signInWithEmailAndPassword(
           email: emailController.text.trim(),
           password: passwordController.text.trim(),
-        ).then((value){
+        )
+            .then((value) {
           myGlobalSnackBarWidget(
             context: context,
             isArabic: isArabic,
             backGroundColor: AppColors.inf_suc_dan_warn_sucess,
             text: S.of(context).signInSuccess,
           );
+          CacheHelper.saveData(
+            key: CacheHelperKeys.uId,
+            value: value.user!.uid,
+          );
           navigateToHomeScreen();
           signinLoading = false;
           return value;
         });
 
-
-
         emailController.clear();
         passwordController.clear();
         emit(SigninSuccessState());
-
-
       } on FirebaseAuthException catch (e) {
-
         signinLoading = false;
         // Handle errors
         String errorMessage = 'An error occurred. Please try again.';
@@ -68,7 +72,6 @@ class SigninCubit extends Cubit<SigninState> {
         } else if (e.code == 'wrong-password') {
           errorMessage = 'Incorrect password.';
         }
-
 
         myGlobalSnackBarWidget(
           context: context,
@@ -81,19 +84,20 @@ class SigninCubit extends Cubit<SigninState> {
     }
   }
 
-
   void navigateToSignup() {
     router.pushReplacementNamed(
       AppRoutesNamed.signUpScreen,
     );
     emit(SigninNavigateToSignupState());
   }
+
   void navigateToHomeScreen() {
     router.pushReplacementNamed(
       AppRoutesNamed.homeScreen,
     );
     emit(SigninNavigateToHomeState());
   }
+
   void navigateToForgetPassword() {
     emit(SigninNavigateToForgetPasswordState());
   }
